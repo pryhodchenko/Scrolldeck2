8121c47792621900d4da680d17bd6c8256b28ae9
Eugene Panteleymonchuk <panteleymonchuk@gmail.com>
2023-09-17 23:30:39 +0300

    Add vite config. Change package commands.

--- Slide canvas.js
+++ Slide canvas.js
@@ -12,26 +12,26 @@ CanvasRenderingContext2D.prototype.roundRect = function (x, y, width, height, ra
     this.quadraticCurveTo(x, y, x + radius, y);
     this.closePath();
   };
-  
+
   // JavaScript code to draw on the canvas
   const customCanvas = document.getElementById('custom-canvas');
+
   const customContext = customCanvas.getContext('2d');
-  
+
   // Set canvas size to cover the entire screen
   customCanvas.width = window.innerWidth;
   customCanvas.height = window.innerHeight;
-  
+
   // Draw the background rectangle
   customContext.fillStyle = 'blue'; // Set the desired background color
   customContext.fillRect(0, 0, customCanvas.width, customCanvas.height);
-  
+
   // Draw the first rounded rectangle
   customContext.fillStyle = 'red'; // Set the desired color
   customContext.roundRect(50, 50, customCanvas.width - 100, customCanvas.height - 100, 15);
   customContext.fill();
-  
+
   // Draw the second rounded rectangle
   customContext.fillStyle = 'green'; // Set the desired color
   customContext.roundRect(100, 100, customCanvas.width - 200, customCanvas.height - 200, 15);
   customContext.fill();
-  
